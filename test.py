e9763da69d4cb8a12f358c1a0387e6d48fe8b4a6
Kawakami Yuki <chuanshangxiongji@gmail.com>
2022-09-03 14:53:45 +0900

    rename : test_flask.py -> app.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from sequence_classification import SequenceClassification
-
-sequence_classification = SequenceClassification()
-
-output = sequence_classification.classification(text='体調はどうですか')
-
-print(output)
-print(output[0]['label'])
-print(output[0]['score'])
